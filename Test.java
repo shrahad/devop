56a85007df1d1d87204dcae56d01e1343d4a92c3
shravantech <35607369+shrahad@users.noreply.github.com>
2024-10-02 16:57:50 +0530

    added addition logic

--- Test.java
+++ Test.java
@@ -1,8 +1,12 @@
 class Test
 {
+  int a =5,
+  int b =10;
 public static void main(string args[])
 {
 system.out.println("hello world")
 system.out.println("thank you")
+  sum = a+b;
+  system.out.println("sum of "+a" and "+b" is "+sum);
 }
 }
